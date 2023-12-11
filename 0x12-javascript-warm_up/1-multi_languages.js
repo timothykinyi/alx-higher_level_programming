9c194fbd8b603b701114b763bce01faf6c27c347
tee <tee@Kinyi.Kinyi>
2023-12-11 23:10:11 +0300

    L

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const The_first_line = 'C is fun';
+const The_second_line =  'Python is cool';
+const The_third_line = 'JavaScript is amazing';
+
+console.log(The_first_line + "\n"+ The_second_line + "\n" + The_third_line );
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,6 @@
+#!/usr/bash/node
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
